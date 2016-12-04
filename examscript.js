d50aed3926681a1072672263d08748cce0e02ec3
Jothi Basu <bosssuperking@gmail.com>
2016-12-04 15:15:58 +0530

    Add files via upload

---
+++ examscript.js
@@ -0,0 +1,187 @@
+
+function hideElem() {
+   
+ document.getElementById("q1").style.visibility = "visible"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem1() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "visible";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+
+}
+function hideElem2() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "visible";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  }
+function hideElem3() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "visible";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+  
+}
+function hideElem4() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "visible";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+  
+}
+function hideElem5() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "visible";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem6() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "visible";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+ 
+}
+function hideElem7() {
+    document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "visible";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem8() {
+
+      document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "visible";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem9() {
+     
+       document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "visible";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem10() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "visible";
+ document.getElementById("q12").style.visibility = "hidden";
+  
+}
+function hideElem11() {
+document.getElementById("q1").style.visibility = "hidden"; 
+ document.getElementById("q2").style.visibility = "hidden";
+ document.getElementById("q3").style.visibility = "hidden";
+ document.getElementById("q4").style.visibility = "hidden";
+ document.getElementById("q5").style.visibility = "hidden";
+ document.getElementById("q6").style.visibility = "hidden";
+ document.getElementById("q7").style.visibility = "hidden";
+ document.getElementById("q8").style.visibility = "hidden";
+ document.getElementById("q9").style.visibility = "hidden";
+ document.getElementById("q10").style.visibility = "hidden";
+ document.getElementById("q11").style.visibility = "hidden";
+ document.getElementById("q12").style.visibility = "visible";
+  
+}
